fff03fbeeff59f2e83e94a6d2c684f58847c7bc7
Rahul-Chahar <rahul.kumar_cs19@gla.ac.in>
2022-03-02 02:44:26 +0530

    handle exception using try and catch block

--- src/main/exceptionHandling.java
+++ src/main/exceptionHandling.java
@@ -7,5 +7,17 @@
 package main;
 
 public class exceptionHandling {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 0;
+        try {
+            int sum = a / b;
+            System.out.println(sum);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        System.out.println("ab crash hou kar dikha");
+    }
 
 }
